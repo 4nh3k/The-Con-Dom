6d0b476ed900df1899fbdd077e89fde9fcb06e36
4nh3k <21520579@gm.uit.edu.com>
2024-11-04 15:19:16 +0700

    update auth

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,18 +1,23 @@
 // src/apis/auth.api.ts
-
+import { URL_LOGIN, URL_REGISTER } from "../constants/endpoints";
 import http from "../utils/http";
 
 const authApi = {
   login: async (credentials: { username: string; password: string }) => {
-    const response = await http.post("/login", credentials);
+    const response = await http.post(URL_LOGIN, credentials);
     // Assuming the token is in response.data.token
     const { token } = response.data; // Adjust this according to your API response structure
     localStorage.setItem("accessToken", token); // Store the token in local storage
     return response;
   },
 
-  register: async (data: { username: string; password: string }) => {
-    const response = await http.post("/register", data);
+  register: async (data: {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }) => {
+    const response = await http.post(URL_REGISTER, data);
     return response;
   },
 };
